de51b58b933d22296c0c575b2a2aa7e1eea73b78
Ola <oladabro@gmail.com>
2021-02-06 21:15:40 +0100

    add function to consants file

--- src/constans.js
+++ src/constans.js
@@ -1,4 +1,16 @@
 export const urlCart = 'http://localhost:8000/yourCart';
 export const urlProducts = 'http://localhost:9000/products';
-export const shippingCost = 23.8;
+export const shippingValue = 23.8;
 export const freeShipping = 0;
+export const freeShipOrderValue = 100;
+
+export const sendOrder = () => {
+  console.log('wysyłam zamówienie');
+  const navBar = document.querySelector('.nav-bar');
+  const cart = document.querySelector('.container');
+  const order = document.querySelector('.order-sent');
+
+  navBar.classList.add('hide');
+  cart.classList.add('hide');
+  order.classList.remove('hide');
+};
