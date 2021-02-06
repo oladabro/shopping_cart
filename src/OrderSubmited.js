d0f729c44f624ae4907f5b3504994633c978012e
Ola <oladabro@gmail.com>
2021-02-06 21:13:06 +0100

    add order submitted message

---
+++ src/OrderSubmited.js
@@ -0,0 +1,11 @@
+const OrderSubmited = () => {
+  return (
+    <div className='message-cotainer'>
+      <div className='order-sent hide'>
+        Your order has been successfully submited
+      </div>
+    </div>
+  );
+};
+
+export default OrderSubmited;
